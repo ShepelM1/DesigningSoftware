f9eda8b8502c160d5dab757adcc8976274dffc82
Shepel Mykola <shepel_ak24@nuwm.edu.ua>
2026-06-16 00:25:20 +0300

    Виправлено конфлікт: додано обидва методи

--- book.py
+++ book.py
@@ -34,6 +34,4 @@ class Catalog(BookSearch):
     def findBooksByTitle(self, title: str):
         return [b for b in self.books if title.lower() in b.title.lower()]
     def get_info(self): pass
-    def get_info(self): pass
-    def get_description(self): pass
     def get_description(self): pass
